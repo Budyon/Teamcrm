1fcc94df6af4ae206d0d52432d8df0974a5aa24f
Budyon <budyonevistep@gmail.com>
2022-11-11 17:12:39 +0400

    fix Each Company task and finished

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use(AuthRouter)
 app.use("/api/v1/auth", AuthRouter)
 app.use("/api/v1/user",auth,userRouter)
 app.use('/api/v1/company',auth,companyRouter)
-app.use('/api/v1/invitations',auth,inviteRouter)
+app.use('/api/v1/invitations',inviteRouter)
 
 // createRole()
 
--- src/routes/invite.ts
+++ src/routes/invite.ts
@@ -2,18 +2,41 @@ import express from 'express'
 import { json } from 'body-parser'
 import { Request, Response } from 'express'
 import nodemailer from 'nodemailer'
+import { auth } from "../util"
 import { Invite } from '../schema/inviteSchema'
 import { User } from '../schema/userSchema'
 import { Company } from '../schema/companySchema'
+import { Role } from '../schema/roleSchema'
 
 const router  = express.Router()
 router.use(json())
 
-router.get('/',(req:Request,res:Response) => {
-    console.log("fsdfsdf")
+router.get('/:id',async(req:Request,res:Response) => {
+    try {
+        const invite = await Invite.findById(req.params.id)
+        if(invite?.active === true) {
+
+            const company = await Company.findById(invite.companyId)
+
+            company?.users.push({
+                user:invite.userId,
+                role:invite.roleId
+            })
+            await company?.save()
+
+            invite.active = false
+            await invite.save()
+        }else {
+            res.json({
+                error:"The invitation has already expired"
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
     
     res.json({
-        aaa:"asafdfsdfdds"
+        Success:"You are already in our company"
     })
 })
 
@@ -26,34 +49,39 @@ let transporter = nodemailer.createTransport({
     }
   })
 
-router.post('/', async(req:Request,res:Response) => {
-    const { companyId,userId,roleId } = req.body
-
-     await Invite.create({
-        companyId,
-        userId,
-        roleId,
-        active:true
-    })
-
-    const user  = await User.findById(userId)
-    console.log(user?.email)
-    const company = await Company.findById(companyId)
-        
-      let info = await transporter.sendMail({
-        from: user?.email,
-        to: "budyonevistep@gmail.com",
-        subject: `Hello from company ${company?.name}`,
-        text: "Invitation",
-        html: "http://localhost:3004/api/v1/invitations/",
-        headers: { 'x-cloudmta-class': 'standard' }
-      })
-
-      console.log("Message sent: %s", info.response)
-
-    res.json({
-        aaa:"asa"
-    })
+router.post('/', auth, async(req:Request,res:Response) => {
+    try {
+        const { companyId,userId,roleId } = req.body
+        const invite = await Invite.create({
+            companyId,
+            userId,
+            roleId,
+            active:true
+        })
+    
+        const role = await Role.findById(roleId)
+        const user  = await User.findById(userId)
+        const company = await Company.findById(companyId)
+    
+        const output = `<h1>Hi, we invite you to our company as a ${role?.name}, if you accept the offer, click the link below</h1>
+        <a href="${`http://localhost:3004/api/v1/invitations/${invite._id}`}">Click here</a>`
+            
+          await transporter.sendMail({
+            from: user?.email,
+            to: "budyonevistep@gmail.com",
+            subject: `Hello from company ${company?.name}`,
+            text: "Invitation",
+            html: output, 
+            headers: { 'x-cloudmta-class': 'standard' }
+          })
+    
+        res.json({
+            Success:"Your invitation has arrived successfully"
+        })
+    } catch (error) {
+        console.log(error)
+    }
+     
 })
  
 export { router as inviteRouter }
